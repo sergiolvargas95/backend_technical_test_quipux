33741f2ca3845f95b505d0e254e232dcbfdea09c
Sergio Vargas <sergiolvargas95@gmail.com>
2026-02-02 14:11:02 -0500

    Initial commit

---
+++ src/main/java/com/playlist/api/PlaylistApiApplication.java
@@ -0,0 +1,13 @@
+package com.playlist.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistApiApplication.class, args);
+	}
+
+}
