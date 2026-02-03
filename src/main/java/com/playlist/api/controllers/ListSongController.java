ef0bf7211ae566d88445ba1b988aabcba91d5ee5
Sergio Vargas <sergiolvargas95@gmail.com>
2026-02-03 02:45:03 -0500

    fix(security): configure CORS policy to allow requests from localhost:4200

--- src/main/java/com/playlist/api/controllers/ListSongController.java
+++ src/main/java/com/playlist/api/controllers/ListSongController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/lists")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class ListSongController {
     private final ListSongService listSongService;
